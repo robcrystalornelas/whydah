55bde8e03c701c3b9dee1d95a84da6f78e14714b
rpecchia <rpecchia@gmail.com>
2017-04-12 15:56:14 -0400

    Code finalized for revision #4

--- R/whydah_7.1.R
+++ R/whydah_7.1.R
@@ -34,15 +34,16 @@ mx_climate_train@lambdas
 px_climate_train <- predict(climate, mx_climate_train, progress='text') #make predictions of habitat suitability can include argument ext=ext
 plot(px_climate_train, main= 'Maxent, raw values')
 
-## DONT CHANGE
-# Forming Confusion Matrix
-test_suit_climate <-extract(px_climate_train, test) # extract predicted values, at all presence points
-test_suit_climate<-na.omit(all_suit_climate)
-ten_thresh_climate_train <- quantile(all_suit_climate, 0.1, na.rm = TRUE)
+# 10% Forming Confusion Matrix
+# Find the 10% training threshold
+train_suit_climate <-extract(px_climate_train, train)
+ten_thresh_climate_train <- quantile(train_suit_climate, 0.1, na.rm = TRUE)
 ten_thresh_climate_train
 
+# Get the suitability values for our test points
+test_suit_climate <-extract(px_climate_train, test) # extract predicted values, at all presence points
+test_suit_climate<-na.omit(test_suit_climate)
 
-## CHANGE
 # Calculating Omission
 pred_binary_climate_test <- test_suit_climate > ten_thresh_climate_train #where are known presence greater than threshold?
 length(pred_binary_climate_test[pred_binary_climate_test==TRUE]) # these are "a" the true positives
@@ -78,22 +79,29 @@ plot(mx_hosts_model_train)
 dim(thin_ptw_with_florida_coords)
 px_hosts_model_train <- predict(hosts, mx_hosts_model_train, progress='text') #make predictions of habitat suitability can include argument ext=ext
 plot(px_hosts_model_train, main= 'Maxent, raw values')
-writeRaster(px_hosts_model_train, filename="hosts_model_for_qgis.tif", format="GTiff", overwrite=TRUE) #exporting a GEOtiff
+writeRaster(px_hosts_model_train, filename="hosts_model_for_qgis_train.tif", format="GTiff", overwrite=TRUE) #exporting a GEOtiff
 
 # 10% Min. Training Pres Threshold
-test_suit_hosts <- extract(px_hosts_model_train, train) #all predicted values, all occs
-test_suit_hosts <- na.omit(test_suit_hosts)
-ten_thresh_hosts_test <- quantile(test_suit_hosts, 0.1, na.rm = TRUE)
-ten_thresh_hosts_test
+train_suit_hosts <- extract(px_hosts_model_train, train) #all predicted values, all occs
+train_suit_hosts <- na.omit(train_suit_hosts)
+ten_thresh_hosts_train <- quantile(train_suit_hosts, 0.1, na.rm = TRUE)
+ten_thresh_hosts_train
 ###
-ten_thresh_hosts_test <- .036
+
 
 # Confusion Matrix
+dim(test)
 test_suitability_hosts <- extract(px_hosts_model_train, test) # extract predicted values, at known presence points
+length(test_suit_hosts)
 test_suitability_hosts <- na.omit(test_suitability_hosts)
-pred_binary_hosts_test <- test_suit_hosts > ten_thresh_hosts_model #where are known presence greater than threshold?
+
+# Calculating omission
+pred_binary_hosts_test <- test_suit_hosts > ten_thresh_hosts_train #where are known presence greater than threshold?
 length(pred_binary_hosts_test[pred_binary_hosts_test==TRUE]) # these are "a" the true positives
+
 length(pred_binary_hosts_test[pred_binary_hosts_test==FALSE]) #these are "c" the false negatives
+which(pred_binary_hosts_test == FALSE)
+head(test_suit_hosts)
 
 background_suitability_hosts <- extract(px_hosts_model, backg_with_florida)
 pred_binary_background_hosts <- background_suitability_hosts > ten_thresh_hosts_model
@@ -101,6 +109,14 @@ pred_binary_background_hosts <- background_suitability_hosts > ten_thresh_hosts_
 length(pred_binary_background_hosts[pred_binary_background_hosts==TRUE]) #these are "b" the false pos
 length(pred_binary_background_hosts[pred_binary_background_hosts==FALSE]) # these are "d" the true neg 
 
+# In order to determine where mis-classified points fall
+head(test)
+dim(na.omit(test))
+head(test_suitability_hosts)
+
+
+write.csv(train, file = "training_whydah_points.csv")
+write.csv(test, file = "test_whydah_points.csv")
 ####################################################################################################
 ################################### MaxEnt for LULC ########################################
 ####################################################################################################
@@ -126,9 +142,10 @@ plot(px_LULC_train, main= 'Maxent, raw values')
 writeRaster(px_LULC_train, filename="LULC_model_for_qgis.tif", format="GTiff", overwrite=TRUE) #exporting a GEOtiff
 
 # 10% Min. Training Pres Threshold
-test_suitability_LULC <- extract(px_LULC_train, test) #all predicted values, all occs
-test_suitability_LULC <- na.omit(test_suitability_LULC)
-ten_thresh_train_LULC <- quantile(test_suitability_LULC, 0.1, na.rm = TRUE)
+train_suitability_LULC <- extract(px_LULC_train, train) #all predicted values, all occs
+train_suitability_LULC <- na.omit(train_suitability_LULC)
+
+ten_thresh_train_LULC <- quantile(train_suitability_LULC, 0.1, na.rm = TRUE)
 ten_thresh_train_LULC
 
 # Confusion Matrix
@@ -167,17 +184,18 @@ plot(px_climate_and_hosts_train, main= 'Maxent, raw values')
 writeRaster(px_climate_and_hosts_train, filename="climate_and_hosts_for_qgis.tif", format="GTiff", overwrite=TRUE) #exporting a GEOtiff
 
 # 10% Min. Training Pres Threshold
-test_suit_climate_and_hosts <- extract(px_climate_and_hosts_train, train) #all predicted values, all occs
-test_suit_climate_and_hosts <- na.omit(test_suit_climate_and_hosts)
-ten_thresh__test_climate_and_hosts <- quantile(test_suit_climate_and_hosts, 0.1, na.rm = TRUE)
-ten_thresh_test_climate_and_hosts
+train_suit_climate_and_hosts <- extract(px_climate_and_hosts_train, train) #all predicted values, all occs
+train_suit_climate_and_hosts <- na.omit(train_suit_climate_and_hosts)
+
+ten_thresh_train_climate_and_hosts <- quantile(train_suit_climate_and_hosts, 0.1, na.rm = TRUE)
+ten_thresh_train_climate_and_hosts
 
 ## Confusion Matrix
 
 ## Omission
 test_suitability_climate_and_hosts <- extract(px_climate_and_hosts_train, test) # extract predicted values, at known presence points
 test_suitability_climate_and_hosts <- na.omit(test_suitability_climate_and_hosts)
-pred_binary_test_climate_and_hosts <- test_suitability_climate_and_hosts > ten_thresh_test_climate_and_hosts #where are known presence greater than threshold?
+pred_binary_test_climate_and_hosts <- test_suitability_climate_and_hosts > ten_thresh_train_climate_and_hosts #where are known presence greater than threshold?
 length(pred_binary_test_climate_and_hosts[pred_binary_test_climate_and_hosts==TRUE]) # these are "a" the true positives
 length(pred_binary_test_climate_and_hosts[pred_binary_test_climate_and_hosts==FALSE]) #these are "c" the false negatives
 
@@ -214,16 +232,15 @@ plot(px_climate_and_LULC_train, main= 'Maxent, raw values')
 writeRaster(px_climate_and_LULC_train, filename="climate_and_LULC_for_qgis.tif", format="GTiff", overwrite=TRUE) #exporting a GEOtiff
 
 # 10% Min. Training Pres Threshold
-test_suitability_climate_and_LULC <- extract(px_climate_and_LULC_train, train) #all predicted values, all occs
-test_suitability_climate_and_LULC <- na.omit(test_suitability_climate_and_LULC)
-ten_thresh_climate_and_LULC <- quantile(test_suitability_climate_and_LULC, 0.1, na.rm = TRUE)
-ten_thresh_climate_and_LULC
+train_suitability_climate_and_LULC <- extract(px_climate_and_LULC_train, train) #all predicted values, all occs
+train_suitability_climate_and_LULC <- na.omit(train_suitability_climate_and_LULC)
+ten_thresh_climate_and_LULC_train <- quantile(train_suitability_climate_and_LULC, 0.1, na.rm = TRUE)
+ten_thresh_climate_and_LULC_train
 
-# Confusion Matrix
 # Omission
 test_suitability_climate_and_LULC <- extract(px_climate_and_LULC_train, test) # extract predicted values, at known presence points
 test_suitability_climate_and_LULC <- na.omit(test_suitability_climate_and_LULC)
-test_binary_climate_and_LULC <- test_suitability_climate_and_LULC > ten_thresh_climate_and_LULC #where are known presence greater than threshold?
+test_binary_climate_and_LULC <- test_suitability_climate_and_LULC > ten_thresh_climate_and_LULC_train #where are known presence greater than threshold?
 length(test_binary_climate_and_LULC[test_binary_climate_and_LULC==TRUE]) # these are "a" the true positives
 length(test_binary_climate_and_LULC[test_binary_climate_and_LULC==FALSE]) #these are "c" the false negatives
 
@@ -262,21 +279,23 @@ writeRaster(px_hosts_and_habitat_train, filename="hosts_and_habitat_for_qgis.tif
 # 10% Min. Training Pres Threshold
 training_suitability_hosts_and_habitat <- extract(px_hosts_and_habitat_train, train) #all predicted values, all occs
 training_suitability_hosts_and_habitat <- na.omit(training_suitability_hosts_and_habitat)
-ten_thresh_hosts_and_habitat <- quantile(training_suitability_hosts_and_habitat, 0.1, na.rm = TRUE)
-ten_thresh_hosts_and_habitat
+ten_thresh_hosts_and_habitat_train <- quantile(training_suitability_hosts_and_habitat, 0.1, na.rm = TRUE)
+ten_thresh_hosts_and_habitat_train
 
 # Confusion Matrix
-training_suitability_hosts_and_habitat <- extract(px_hosts_and_habitat, thin_ptw_with_florida_coords) # extract predicted values, at known presence points
-training_suitability_hosts_and_habitat <- na.omit(training_suitability_hosts_and_habitat)
-pred_binary_hosts_and_habitat <- training_suitability_hosts_and_habitat > ten_thresh_hosts_and_habitat #where are known presence greater than threshold?
-length(pred_binary_hosts_and_habitat[pred_binary_hosts_and_habitat==TRUE]) # these are "a" the true positives
-length(pred_binary_hosts_and_habitat[pred_binary_hosts_and_habitat==FALSE]) #these are "c" the false negatives
+# Omission Error
+test_suitability_hosts_and_habitat <- extract(px_hosts_and_habitat, test) # extract predicted values, at known presence points
+test_suitability_hosts_and_habitat <- na.omit(test_suitability_hosts_and_habitat)
+pred_binary_hosts_and_habitat_test <- test_suitability_hosts_and_habitat > ten_thresh_hosts_and_habitat_train #where are known presence greater than threshold?
+length(pred_binary_hosts_and_habitat_test[pred_binary_hosts_and_habitat_test==TRUE]) # these are "a" the true positives
+length(pred_binary_hosts_and_habitat_test[pred_binary_hosts_and_habitat_test==FALSE]) #these are "c" the false negatives
 
-background_suitability_hosts_and_habitat <- extract(px_hosts_and_habitat, backg_with_florida)
-pred_binary_background_hosts_and_habitat <- background_suitability_hosts_and_habitat > ten_thresh_hosts_and_habitat
-
-length(pred_binary_background_hosts_and_habitat[pred_binary_background_hosts_and_habitat==TRUE]) #these are "b" the false pos
-length(pred_binary_background_hosts_and_habitat[pred_binary_background_hosts_and_habitat==FALSE]) # these are "d" the true neg 
+# Comission error
+# background_suitability_hosts_and_habitat <- extract(px_hosts_and_habitat, backg_with_florida)
+# pred_binary_background_hosts_and_habitat <- background_suitability_hosts_and_habitat > ten_thresh_hosts_and_habitat
+# 
+# length(pred_binary_background_hosts_and_habitat[pred_binary_background_hosts_and_habitat==TRUE]) #these are "b" the false pos
+# length(pred_binary_background_hosts_and_habitat[pred_binary_background_hosts_and_habitat==FALSE]) # these are "d" the true neg 
 
 
 ####################################################################################################
@@ -292,36 +311,38 @@ mx_climate_hosts_LULC_florida@results
 names(climate_and_hosts_and_LULC)
 
 # all occurrences
-mx_climate_hosts_LULC_florida_full <- maxent(climate_and_hosts_and_LULC, thin_ptw_with_florida_coords, a=backg_with_florida, factors = "band1",
+mx_climate_hosts_LULC_florida_train <- maxent(climate_and_hosts_and_LULC, train, a=backg_with_florida, factors = "band1",
                                              args=c('responsecurves=TRUE',
                                                     'writebackgroundpredictions=TRUE'))
 
 
-plot(mx_climate_hosts_LULC_florida_full)
-mx_climate_hosts_LULC_florida_full@results
-mx_climate_hosts_LULC_florida_full@lambdas
+plot(mx_climate_hosts_LULC_florida_train)
+mx_climate_hosts_LULC_florida_train@results
+mx_climate_hosts_LULC_florida_train@lambdas
 
-px_climate_hosts_LULC_florida <- predict(climate_and_hosts_and_LULC, mx_climate_hosts_LULC_florida_full, progress='text') #make predictions of habitat suitability can include argument ext=ext
-plot(px_climate_hosts_LULC_florida, main= 'Maxent, raw values')
-writeRaster(px_climate_hosts_LULC_florida, filename="climate_hosts_LULC_with_florida_for_qgis.tif", format="GTiff", overwrite=TRUE) #exporting a GEOtiff
+px_climate_hosts_LULC_train <- predict(climate_and_hosts_and_LULC, mx_climate_hosts_LULC_florida_train, progress='text') #make predictions of habitat suitability can include argument ext=ext
+plot(px_climate_hosts_LULC_train, main= 'Maxent, raw values')
+writeRaster(px_climate_hosts_LULC_train, filename="climate_hosts_LULC_with_florida_for_qgis.tif", format="GTiff", overwrite=TRUE) #exporting a GEOtiff
 
 # 10% Min. Training Pres Threshold
-training_suitability_climate_host_LULC <- extract(px_climate_hosts_LULC_florida, train) #all predicted values, all occs
+training_suitability_climate_host_LULC <- extract(px_climate_hosts_LULC_train, train) #all predicted values, all occs
 training_suitability_climate_host_LULC <- na.omit(training_suitability_climate_host_LULC)
-ten_thresh_climate_host_LULC <- quantile(training_suitability_climate_host_LULC, 0.1, na.rm = TRUE)
-ten_thresh_climate_host_LULC
+ten_thresh_climate_host_LULC_train <- quantile(training_suitability_climate_host_LULC, 0.1, na.rm = TRUE)
+ten_thresh_climate_host_LULC_train
 
 # Confusion Matrix
-training_suitability_climate_hosts_LULC <- extract(px_climate_hosts_LULC_florida, thin_ptw_with_florida_coords) # extract predicted values, at known presence points
-training_suitability_climate_hosts_LULC <- na.omit(training_suitability_climate_hosts_LULC)
-pred_binary_climate_hosts_LULC <- training_suitability_climate_hosts_LULC > ten_thresh_climate_host_LULC #where are known presence greater than threshold?
-length(pred_binary_climate_hosts_LULC[pred_binary_climate_hosts_LULC==TRUE]) # these are "a" the true positives
-length(pred_binary_climate_hosts_LULC[pred_binary_climate_hosts_LULC==FALSE]) #these are "c" the false negatives
+# Omission Erro
+test_suitability_climate_hosts_LULC <- extract(px_climate_hosts_LULC_train, test) # extract predicted values, at known presence points
+test_suitability_climate_hosts_LULC <- na.omit(test_suitability_climate_hosts_LULC)
+pred_binary_climate_hosts_LULC_train <- test_suitability_climate_hosts_LULC > ten_thresh_climate_host_LULC_train #where are known presence greater than threshold?
+length(pred_binary_climate_hosts_LULC_train[pred_binary_climate_hosts_LULC_train==TRUE]) # these are "a" the true positives
+length(pred_binary_climate_hosts_LULC_train[pred_binary_climate_hosts_LULC_train==FALSE]) #these are "c" the false negatives
 
-background_suitability_climate_hosts_LULC <- extract(px_climate_hosts_LULC_florida, backg_with_florida)
-pred_binary_background_climate_hosts_LULC <- background_suitability_climate_hosts_LULC > ten_thresh_climate_host_LULC
-
-length(pred_binary_background_climate_hosts_LULC[pred_binary_background_climate_hosts_LULC==TRUE]) #these are "b" the false pos
-length(pred_binary_background_climate_hosts_LULC[pred_binary_background_climate_hosts_LULC==FALSE]) # these are "d" the true neg 
+# Comission Error
+# background_suitability_climate_hosts_LULC <- extract(px_climate_hosts_LULC_florida, backg_with_florida)
+# pred_binary_background_climate_hosts_LULC <- background_suitability_climate_hosts_LULC > ten_thresh_climate_host_LULC
+# 
+# length(pred_binary_background_climate_hosts_LULC[pred_binary_background_climate_hosts_LULC==TRUE]) #these are "b" the false pos
+# length(pred_binary_background_climate_hosts_LULC[pred_binary_background_climate_hosts_LULC==FALSE]) # these are "d" the true neg 
 
 
